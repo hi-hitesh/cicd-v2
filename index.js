1a82994b71b686349465b8b2f0da17cef925ca87
hitesh <admin@Admins-MacBook-Pro.local>
2023-02-13 12:06:14 +0530

    Push iniital commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 3005;
+
+app.get('/status', (req, res) => {
+  return res.send('Ok');
+});
+
+app.get('/', (req, res) => {
+  return res.send(`Hello World! from', ${process.env.ENV}`);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
